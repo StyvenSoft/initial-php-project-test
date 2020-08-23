c3f9ecba888ca4e6a31891e875f9649622c0504d
Steveen <steveen.echeverri@gmail.com>
2020-08-22 23:55:20 -0500

    Created variables practics

---
+++ UsingVariables.php
@@ -0,0 +1,29 @@
+<?php
+// Write your code below:
+
+$favorite_food = "Red curry with eggplant, green beans, and peanuts";
+echo $favorite_food; 
+// Prints: Red curry with eggplant, green beans, and peanuts
+
+$dog_name = "Tadpole";
+echo $dog_name; 
+// Prints: Tadpole
+
+$dog_name = "Tadpole";
+echo "My dog is named " . $dog_name; 
+// Prints: My dog is named Tadpole
+
+$dog_name = "Tadpole";
+$favorite_food = "salmon";
+$color = "brown";
+
+echo "I have a " . $color . " dog named " . $dog_name . " and her favorite food is " . $favorite_food . ".";
+// Prints: I have a brown dog named Tadpole and her favorite food is salmon.
+
+
+$name = "Steveen!";
+$language = "Spanish.";
+  
+
+echo "I love concatenating " . $name;
+echo "\nI love " . $language;
