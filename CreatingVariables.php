5e0e186e028bb37bd17a8d17ad0b6f2efc8b5989
Steveen <steveen.echeverri@gmail.com>
2020-08-22 23:51:25 -0500

    Create variables and string concatenation

---
+++ CreatingVariables.php
@@ -0,0 +1,11 @@
+<?php
+// Write your code below:
+
+$my_name = "Steveen Echeverri";
+
+$mood = ":)";
+$favorite_food = "Red curry with eggplant";
+
+$silly = "without common sense, absurd, ridiculous";
+
+$example = "This " . "is " ."an " . "example.";
---
+++ StringConcatenation.php
@@ -0,0 +1,11 @@
+<?php
+// Write your code below:   
+
+
+  //echo "\nMy name is:" 
+
+  echo "one" . "two"; // Prints: onetwo
+
+  echo "one" . " " . "two" . " " . "three"; // Prints: one two three
+
+  echo "\nMy name is:" . " Steveen Echeverri";
