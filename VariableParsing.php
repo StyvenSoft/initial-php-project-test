0d59f7fdccfa4d044446364527dbd80f947d5ade
Steveen <steveen.echeverri@gmail.com>
2020-08-23 18:31:53 -0500

    Created variables parsing

---
+++ VariableParsing.php
@@ -0,0 +1,19 @@
+<?php
+$dog_name = "Tadpole";
+$favorite_food = "salmon";
+$color = "brown";
+
+echo "I have a $color dog named $dog_name and her favorite food is $favorite_food.";
+// Prints: I have a brown dog named Tadpole and her favorite food is salmon.
+
+// Fill in the blanks in the code below:
+$noun = "actor";
+$adjective = "silly";
+$verb = "travel";
+
+echo "The world's most beloved $noun was very $adjective and loved to $verb every single day.";
+
+
+//Fix the code below and uncomment it:
+
+echo "\nI have always been obsessed with ${noun}s. I'm ${adjective}ish. I'm always ${verb}ing.";
