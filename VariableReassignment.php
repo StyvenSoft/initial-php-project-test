a9c6635eab2c57aead840b334728d65289a9d692
Steveen <steveen.echeverri@gmail.com>
2020-08-23 18:43:07 -0500

    Exercise varaibles Reassignment

---
+++ VariableReassignment.php
@@ -0,0 +1,27 @@
+<?php
+
+$favorite_food = "Red curry with eggplant";
+echo $favorite_food; // Prints: Red curry with eggplant
+
+// Reassign the value of $favorite_food to a new string
+$favorite_food = "Pizza"; 
+echo $favorite_food; // Prints: Pizza
+
+
+$movie = "Scare";
+// Add your code here:
+$old_favorite = $movie; 
+
+
+  echo "I'm a fickle person, my favorite movie used to be $movie.";
+  
+// Add a statement here:
+  $movie = "First I had this value.";
+  
+  echo "\nBut now my favorite is $movie.";
+  
+// Add a statement below:
+
+$movie = "But now I have this one!";
+  
+echo "$old_favorite";
