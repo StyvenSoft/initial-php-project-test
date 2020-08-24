49c9488fb15b05bff4a0c6ec99317e39b9b53ff7
Steveen <steveen.echeverri@gmail.com>
2020-08-23 20:08:53 -0500

    Operators for performing

---
+++ MultiplicationDivision.php
@@ -0,0 +1,20 @@
+<?php
+
+echo 2 * 3; // Prints: 6
+echo -21 / 7; // Prints: -3
+
+
+$num_cookies = 24;
+$num_friends = 7;
+$cookies_per_friend = $num_cookies / $num_friends;
+echo $cookies_per_friend; // Prints: 3.4285714285714
+
+
+$num_languages = 4;
+$months = 11;
+  
+$days = $months * 16;
+
+$days_per_language = $days / $num_languages;
+
+echo $days_per_language;
