85804f7b7edb5655fbeec618cce494d6e278a013
Steveen <steveen.echeverri@gmail.com>
2020-08-24 12:09:39 -0500

    Completed lesson 1 folder

--- MathematicalOperators.php
+++ MathematicalOperators.php
@@ -4,3 +4,27 @@ $bike_cost = 75;
 $savings = $savings - $bike_cost;
 echo $savings; // Prints: 725
 
+$age = 89; 
+$age++;
+echo $age; // Prints: 90
+
+$days_til_vacation = 7; 
+$days_til_vacation--;
+echo $days_til_vacation; // Prints: 6
+
+$my_num = -19273645.862;
+
+	$answer = $my_num;
+
+	$answer += 2;
+	$answer *= 2;
+	$answer -= 2;
+	$answer /= 2;
+
+	$answer -= $my_num;
+
+  echo "Look! It still works!\n";
+
+	echo $answer;
+
+  echo "\nCool?\nIt's a little cool, right?";
