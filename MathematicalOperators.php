31a2d6fe0c5ad935a6126308345706faf0e922e4
Steveen <steveen.echeverri@gmail.com>
2020-08-23 22:34:02 -0500

    Practics operators numbers

---
+++ MathematicalOperators.php
@@ -0,0 +1,6 @@
+<?php
+$savings = 800;
+$bike_cost = 75;
+$savings = $savings - $bike_cost;
+echo $savings; // Prints: 725
+
---
+++ OrderOperations.php
@@ -0,0 +1,10 @@
+<?php
+
+echo 2 + 3 + 4 + 5 - 1.1; // Prints: 12.9
+echo 2 * 9 / 6; // Prints: 3
+
+echo 1 + 3 * 9; // Prints: 28
+
+echo (1 + 3) * 9; // Prints: 36
+
+echo 94 - 4.25 + 7 - 23.50 * 1.2 + 20 / 4; // Prints: 92.35 
