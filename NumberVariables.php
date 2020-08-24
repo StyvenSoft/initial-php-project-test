5ddee178a75ea7b8ed30b963aeb578e69d9da21c
Steveen <steveen.echeverri@gmail.com>
2020-08-23 19:50:10 -0500

    Number operators on variables

---
+++ NumberVariables.php
@@ -0,0 +1,15 @@
+<?php
+
+$tadpole_age = 7;
+$lily_age = 6; 
+$age_difference = $tadpole_age - $lily_age;
+echo $age_difference; // Prints 1
+
+$favorite_num = 22;
+echo $favorite_num + 1; // Prints 23
+echo $favorite_num; //Prints 22
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+  
+  echo $last_month - $this_month;
