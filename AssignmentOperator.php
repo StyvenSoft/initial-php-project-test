62d5b000bd3fb24c0f243548fd9fdd09700bab2d
Steveen <steveen.echeverri@gmail.com>
2020-08-23 19:00:15 -0500

    Completed assinment Operator

---
+++ AssignmentOperator.php
@@ -0,0 +1,18 @@
+<?php
+
+$full_name = "Aisle";
+$full_name .= " Nevertell";
+echo $full_name; // Prints: Aisle Nevertell
+
+  echo "I'm going on a picnic!";
+
+  $sentence = "\nI'm going on a picnic, and I'm taking apples";
+
+  echo $sentence;
+
+$example = "Hello";
+$part2 = ", World";
+$example .= $part2;
+$part3 = "!";
+$example .= $part3;
+echo $example; // Prints: Hello, World!
