71a8395031476007623cf84964404b22639d72be
Steveen <steveen.echeverri@gmail.com>
2020-09-17 18:14:49 -0500

    Created project test php calculator simple

---
+++ PRACTICES/PHPCalculator/division.php
@@ -0,0 +1,9 @@
+<html>
+
+<body>
+    <!--Your code goes here-->
+
+    <a href="index.php">Reset</a>
+</body>
+
+</html>
---
+++ PRACTICES/PHPCalculator/index.php
@@ -0,0 +1,20 @@
+<html>
+
+<body>
+    <h3>Addition</h3>
+    <form action="addition.php">
+        First Number: <input type="number"><br>
+        Second Number: <input type="number"><br>
+        <button type="submit">Add</button><br>
+    </form>
+    <h3>Division</h3>
+    <form action="division.php">
+        Numerator: <input type="number"><br>
+        Denominator: <input type="number"><br>
+        <button type="submit">Divide</button><br>
+    </form>
+
+    <a href="index.php">Reset</a>
+</body>
+
+</html>
