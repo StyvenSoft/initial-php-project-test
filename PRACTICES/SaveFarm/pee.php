577058993cd3ca754a8a3e46e8ae54e0d28703a0
Steveen <steveen.echeverri@gmail.com>
2020-09-24 18:13:56 -0500

    Write more helper functions

--- PRACTICES/SaveFarm/pee.php
+++ PRACTICES/SaveFarm/pee.php
@@ -1,11 +1,14 @@
 <?php
-  
-function pee(){
+
+function pee()
+{
 	// Write your code here:
-  
-  
-  
-  
-  
-  
+	global $location, $needs_to_pee;
+	
+	if ($location === "bathroom" || $location === "woods") {
+		echo "You relieve yourself.\n";
+		$needs_to_pee = FALSE;
+	} else {
+		echo "Are you crazy? You can't pee here!\n";
+	}
 }
